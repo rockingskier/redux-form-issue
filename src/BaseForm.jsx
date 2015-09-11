dc994631ae31e4266389614f904cc041dd236459
Erik Rasmussen <rasmussenerik@gmail.com>
2015-09-11 15:58:29 +0200

    added some field flags to better visualize state

--- src/BaseForm.jsx
+++ src/BaseForm.jsx
@@ -10,6 +10,9 @@ class BaseForm extends Component {
         {Object.keys(fields).map(fieldName => <div key={fieldName}>
           <label>{fieldName}</label>
           <input type="text" {...fields[fieldName]}/>
+          {fields[fieldName].touched && 'T' }
+          {fields[fieldName].active && 'A' }
+          {fields[fieldName].visited && 'V' }
         </div>)}
       </div>
     );
